52bfbc22cdd52cef3488a8fd61fe895da0f54426
Ravish Bisht <ravishbisht85@gmail.com>
2023-11-21 17:17:11 +0530

    setup configured

--- src/app.js
+++ src/app.js
@@ -10,10 +10,15 @@ app.use(
     credentials: true, //not imp {}
   })
 );
-
 app.use(express.json({ limit: "16kb" })); //limit not imp
 app.use(express.urlencoded({ extended: true, limit: "16kb" })); //{} extended not imp
 app.use(express.static("public"));
 app.use(cookieParser());
+
+//routes import
+import userRouter from "./routes/user.routes.js";
+
+//routes declaration
+app.use("/api/v1/users", userRouter);
 
 export { app };
