7bc1eaf332eb4c777f449aac309c6ff8a0cdf1a6
Ravish Bisht <ravishbisht85@gmail.com>
2023-12-30 15:38:19 +0530

    bug fixes

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -10,6 +10,7 @@ import {
   updateUserCoverImage,
   getUserChannelProfile,
   getWatchHistory,
+  updateAccountDetails,
 } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
